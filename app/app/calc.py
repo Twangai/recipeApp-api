d5e11bf38907805bd17e1934914871bbc9d8d18d
Twangai <wangaityrus18@gmail.com>
2025-05-27 13:27:11 +0300

    update

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator fuctions
+"""
+
+def add(x, y):
+    """Add x and y and return results."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return results."""
+    return y-x
